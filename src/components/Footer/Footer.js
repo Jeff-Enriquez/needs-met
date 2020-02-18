c1f87e6878f5137563db96ecff2d3e9c51282e6b
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-02-18 13:40:58 -0800

    Style footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,11 +5,26 @@ function Footer() {
   return (
     <>
     <nav className='footer-nav'>
-      <img src={process.env.PUBLIC_URL + '/images/home.jpeg'} alt='home' />
-      <img src={process.env.PUBLIC_URL + '/images/message.png'} alt='message'/>
-      <img src={process.env.PUBLIC_URL + '/images/plus.png'} alt='plus'/>
-      <img src={process.env.PUBLIC_URL + '/images/bag.png'} alt='bag'/>
-      <img src={process.env.PUBLIC_URL + '/images/hamburger.png'} alt='hamburger-icon'/>
+      <div>
+        <img src={process.env.PUBLIC_URL + '/images/home.jpeg'} alt='home' />
+        <p className='footer-p'>Help Others</p>
+      </div>
+      <div>
+        <img src={process.env.PUBLIC_URL + '/images/message.png'} alt='message'/>
+        <p className='footer-p'>Messages</p>
+      </div>
+      <div>
+        <img src={process.env.PUBLIC_URL + '/images/plus.png'} alt='plus'/>
+        <p className='footer-p'>Add Need</p>
+      </div>
+      <div>
+        <img src={process.env.PUBLIC_URL + '/images/bag.png'} alt='bag'/>
+        <p className='footer-p'>My Needs</p>
+      </div>
+      <div>
+        <img src={process.env.PUBLIC_URL + '/images/hamburger.png'} alt='hamburger-icon'/>
+        <p className='footer-p'>More</p>
+      </div>
     </nav>
     </> 
   );
