5e7b69fa11838129b43e1238a3d2081da0fcf8ca
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-02-24 14:48:54 -0800

    Display login/signup errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, useHistory } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -25,34 +25,24 @@ class App extends Component {
     }))
   }
 
-  currentPage = (page) => {
-    this.setState({
-      currentPage: page,
-     })
-  }
-
   render() {
-    if(!this.state.user.isLoggedIn){
-      return <Login />
-    } else {
-      return (
-        <>
-          <Header />
-          <Switch>
-            <Route exact path='/' render={() => <Needs
-              isUnmet={this.state.isUnmet} 
-              toggleIsUnmet={this.toggleIsUnmet}
-            />} />
-            <Route exact path='/messages' component={Messages} />
-            <Route exact path='/add-a-need' component={AddANeed} />
-            <Route exact path='/my-needs' component={MyNeeds} />
-            <Route exact path='/more' component={More} />
-            <Route exact path='/login' component={Login} />
-          </Switch>
-          <Footer currentPage={this.currentPage} />
-        </>
-      );
-    }
+    return (
+      <>
+        {/* <Header /> */}
+        <Switch>
+          <Route exact path='/' render={() => <Needs
+            isUnmet={this.state.isUnmet} 
+            toggleIsUnmet={this.toggleIsUnmet}
+          />} />
+          <Route exact path='/messages' component={Messages} />
+          <Route exact path='/add-a-need' component={AddANeed} />
+          <Route exact path='/my-needs' component={MyNeeds} />
+          <Route exact path='/more' component={More} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+        {/* <Footer currentPage={this.currentPage} /> */}
+      </>
+    );
   }
 }
 
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styles from './Login.module.css';
 import Firebase from '../Firebase/firebase';
+import { Redirect } from 'react-router-dom';
 
 class Login extends Component {
   state = {
@@ -8,10 +9,12 @@ class Login extends Component {
     loginPassword: '',
     signupName: '',
     signupEmail: '',
-    signupPassword: '',
+    signupPassword1: '',
+    signupPassword2: '',
     isLogin: true,
     isAuth: false,
-    error: null,
+    signupError: null,
+    loginError: null,
   }
 
   switchIsLogin = () => {
@@ -37,34 +40,43 @@ class Login extends Component {
       this.setState({
         isAuth: true,
       });
-    } catch (error) {
-      console.log(error)
-      console.log('hit')
+    } catch (loginError) {
+      this.setState({
+        loginError,
+      });
     }
   };
 
   handleSignupForm = async e => {
-    const { signupName, signupEmail, signupPassword } = this.state;
+    const { signupName, signupEmail, signupPassword1, } = this.state;
     e.preventDefault();
     try {
       await Firebase.doCreateUserWithEmailAndPassword(
         signupEmail,
-        signupPassword,
+        signupPassword1,
       );
       // this.props.doSetCurrentUser({
       //   name: signupName,
       //   email: signupEmail,
       // });
-      this.setState({ isAuth: true });
-    } catch (error) {
+      // this.setState({ isAuth: true });
+    } catch (signupError) {
       this.setState({
-        error,
+        signupError,
       });
     }
   };
 
   render(){
-    const { isLogin, loginEmail, loginPassword, signupName, signupEmail, signupPassword } = this.state
+    const { isLogin, loginEmail, loginPassword, signupName, signupEmail, signupPassword1, signupPassword2, isAuth, signupError, loginError } = this.state
+    if (isAuth) {
+      return <Redirect to='/' />
+    }
+    const isInvalid =
+      signupPassword1 !== signupPassword2 ||
+      signupPassword1 === '' ||
+      signupEmail === '' ||
+      signupName === ''
 
     return (
       <main className={styles.main}>
@@ -73,27 +85,30 @@ class Login extends Component {
             <h2 className={styles.formTitle} onClick={isLogin ? () => {} : this.switchIsLogin }> 
               <span className={styles.span} >or</span>Log in
             </h2>
-            <form onClick={() => this.handleLoginForm}>
+            <form onSubmit={this.handleLoginForm}>
               <div className={styles.formHolder}>
-                <input type="email" className={styles.input} placeholder="Email" name='loginEmail' value={loginEmail} onChange={this.handleChange} required />
-                <input type="password" className={styles.input} placeholder="Password" name='loginPassword' value={loginPassword} onChange={this.handleChange} required />
+                <input type="email" className={styles.input} placeholder="Email" name='loginEmail' value={loginEmail} onChange={this.handleChange} />
+                <input type="password" className={styles.input} placeholder="Password" name='loginPassword' value={loginPassword} onChange={this.handleChange} />
               </div>
               <button type='submit' className={styles.submitBtn}>Log in</button>
             </form>
+            {loginError && <span style={{ color: 'red' }}>{loginError.message}</span>}
           </div>
           <div className={isLogin ? `${styles.signup} ${styles.slideUp}` : styles.signup}>
             <div className={styles.center}>
               <h2 className={styles.formTitle} onClick={isLogin ? this.switchIsLogin : () => {}}>
                 <span className={styles.span} >or</span>Sign up
               </h2>
-              <form onClick={this.handleSignupForm}>
+              <form onSubmit={this.handleSignupForm}>
                 <div className={styles.formHolder}>
-                  <input type="text" className={styles.input} placeholder="Name" name='signupName' value={signupName} onChange={this.handleChange} />
-                  <input type="email" className={styles.input} placeholder="Email" name='signupEmail' value={signupEmail} onChange={this.handleChange} />
-                  <input type="password" className={styles.input} placeholder="Password" name='signupPassword' value={signupPassword} onChange={this.handleChange} />
+                  <input type="text" className={styles.input} placeholder="Name" name='signupName' value={signupName} onChange={this.handleChange}  />
+                  <input type="email" className={styles.input} placeholder="Email" name='signupEmail' value={signupEmail} onChange={this.handleChange}  />
+                  <input type="password" className={styles.input} placeholder="Password" name='signupPassword1' value={signupPassword1} onChange={this.handleChange}  />
+                  <input type="password" className={styles.input} placeholder="Confirm Password" name='signupPassword2' value={signupPassword2} onChange={this.handleChange}  />
                 </div>  
-              <button type='submit' className={styles.submitBtn}>Sign up</button>
+                <button type='submit' disabled={isInvalid} className={styles.submitBtn}>Sign up</button>
               </form>
+              {signupError && <span style={{ color: 'red' }}>{signupError.message}</span>}
             </div>
           </div>
         </div>
