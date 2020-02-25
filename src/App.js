776f31efb58b5ce415ec6a5233d9c88c14827ff9
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-02-24 16:47:49 -0800

    Style header, footer, and 'needs'

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import More from './components/More';
 class App extends Component {
   state = {
     currentUser: {},
-    isLoggedIn: false,
+    isLoggedIn: true,
     isUnmet: false,
   }
 
--- src/components/Needs/index.js
+++ src/components/Needs/index.js
@@ -4,14 +4,16 @@ import styles from './Needs.module.css'
 function Needs({ isUnmet, toggleIsUnmet }) {
   return (
     <>
-      <div className={styles.btnContainer}>
-        {isUnmet ? (
-            <><button className={`${styles.button} ${styles.lightblue}`} >Unmet Needs</button>
-            <button className={styles.button} onClick={toggleIsUnmet}>Met Needs</button></>
-        ) : (
-            <><button className={styles.button} onClick={toggleIsUnmet}>Unmet Needs</button>
-            <button className={`${styles.button} ${styles.lightblue}`} id='needs-selected'>Met Needs</button></>
-        )}
+      <div className={styles.fixBtns}>
+        <div className={styles.btnContainer}>
+          {isUnmet ? (
+              <><button className={`${styles.button} ${styles.lightblue}`} >Unmet Needs</button>
+              <button className={styles.button} onClick={toggleIsUnmet}>Met Needs</button></>
+          ) : (
+              <><button className={styles.button} onClick={toggleIsUnmet}>Unmet Needs</button>
+              <button className={`${styles.button} ${styles.lightblue}`} id='needs-selected'>Met Needs</button></>
+          )}
+        </div>
       </div>
       <main className={styles.main}>
         <div className={styles.needsContainer}>
@@ -48,6 +50,14 @@ function Needs({ isUnmet, toggleIsUnmet }) {
         </div>
         <div className={styles.needsContainer}>
           <img src={process.env.PUBLIC_URL + '/images/profile2.jpeg'} alt='profile'/>
+          <p>This is a comment of what my posted need is.</p>
+        </div>
+        <div className={styles.needsContainer}>
+          <img src={process.env.PUBLIC_URL + '/images/profile1.jpg'} alt='profile'/>
+          <p>This is a comment of what my posted need is.</p>
+        </div>
+        <div className={styles.needsContainer}>
+          <img src={process.env.PUBLIC_URL + '/images/profile1.jpg'} alt='profile'/>
           <p>This is a comment of what my posted need is.</p>
         </div>
       </main>
