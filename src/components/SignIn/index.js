9d7fdd750a490076068008ddacacc218d8e0ab0c
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-02-26 08:29:25 -0800

    Refactor: change 'signin' component from class to hook

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,54 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import styles from './Login.module.css';
 import Firebase from '../Firebase/firebase';
 import { Redirect } from 'react-router-dom';
 import Login from '../Login';
 import Signup from '../Signup';
 
-class Signin extends Component {
-  state = {
-    isLogin: true,
-    isAuth: false,
-    signupError: null,
+const Signin = ({ doSetCurrentUser, }) => {
+  const [isLogin, setIsLogin] = useState(true)
+  const [isAuth, setIsAuth] = useState(false)
+
+  const doAuth = () => {
+    setIsAuth(true)
   }
 
-  switchIsLogin = () => {
-    this.setState(state => ({
-      isLogin: !state.isLogin
-    }))
+  if (isAuth) {
+    return <Redirect to='/' />
   }
 
-  doAuth = () => {
-    this.setState({ isAuth: true });
-  }
-
-  render(){
-    const { isLogin, isAuth, } = this.state
-    if (isAuth) {
-      return <Redirect to='/' />
-    }
-
-    return (
-      <main className={styles.main}>
-        <div className={styles.formStructor}>
-          <div className={isLogin ? styles.login : `${styles.login} ${styles.slideUp}`}>
-            <h2 className={styles.formTitle} onClick={isLogin ? () => {} : this.switchIsLogin }> 
-              <span className={styles.span} >or</span>Log in
+  return (
+    <main className={styles.main}>
+      <div className={styles.formStructor}>
+        <div className={isLogin ? styles.login : `${styles.login} ${styles.slideUp}`}>
+          <h2 className={styles.formTitle} onClick={isLogin ? () => {} : () => setIsLogin(!isLogin) }> 
+            <span className={styles.span} >or</span>Log in
+          </h2>
+          <Login doSetCurrentUser={doSetCurrentUser} doAuth={doAuth}/>
+        </div>
+        <div className={isLogin ? `${styles.signup} ${styles.slideUp}` : styles.signup}>
+          <div className={styles.center}>
+            <h2 className={styles.formTitle} onClick={isLogin ? () => setIsLogin(!isLogin) : () => {}}>
+              <span className={styles.span} >or</span>Sign up
             </h2>
-            <Login doSetCurrentUser={this.props.doSetCurrentUser} doAuth={this.doAuth}/>
-          </div>
-          <div className={isLogin ? `${styles.signup} ${styles.slideUp}` : styles.signup}>
-            <div className={styles.center}>
-              <h2 className={styles.formTitle} onClick={isLogin ? this.switchIsLogin : () => {}}>
-                <span className={styles.span} >or</span>Sign up
-              </h2>
-              <Signup doSetCurrentUser={this.props.doSetCurrentUser} doAuth={this.doAuth}/>
-            </div>
+            <Signup doSetCurrentUser={doSetCurrentUser} doAuth={doAuth}/>
           </div>
         </div>
-      </main>
-    );
-  }
+      </div>
+    </main>
+  );
 }
 
 export default Signin;
