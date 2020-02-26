bc878219c719ba8188c8a884bf1193d2c4db09b0
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-02-26 11:11:12 -0800

    Add user to collection on signup

--- src/components/SignIn/Signup/index.js
+++ src/components/SignIn/Signup/index.js
@@ -3,7 +3,8 @@ import Firebase from '../../../services/Firebase/firebase';
 import styles from '../Login.module.css';
 
 const Signup = ({ doSetCurrentUser, doAuth }) => {
-  const [name, setName] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
   const [email, setEmail] = useState('')
   const [password1, setPassword1] = useState('')
   const [password2, setPassword2] = useState('')
@@ -13,15 +14,23 @@ const Signup = ({ doSetCurrentUser, doAuth }) => {
       password1 !== password2 ||
       password1 === '' ||
       email === '' ||
-      name === ''
+      firstName === '' ||
+      lastName === ''
 
   const handleForm = async e => {
     e.preventDefault();
     if(isInvalid){ return } else {
     try {
-      await Firebase.doCreateUserWithEmailAndPassword(email, password1);
+      const { user } = await Firebase.doCreateUserWithEmailAndPassword(email, password1);
+      await Firebase.database.collection('Users').add({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password1,
+        uid: user.uid,
+      })
       doSetCurrentUser({
-        name, email,
+        email,
       });
       doAuth();
     } catch (err) {
@@ -33,7 +42,8 @@ const Signup = ({ doSetCurrentUser, doAuth }) => {
     <>
       <form onSubmit={handleForm}>
         <div className={styles.formHolder}>
-          <input type="text" className={styles.input} placeholder="Name" name='name' value={name} onChange={e => setName(e.target.value)}  />
+          <input type="text" className={styles.input} placeholder="First Name" name='firstName' value={firstName} onChange={e => setFirstName(e.target.value)}  />
+          <input type="text" className={styles.input} placeholder="Last Name" name='lastName' value={lastName} onChange={e => setLastName(e.target.value)}  />
           <input type="email" className={styles.input} placeholder="Email" name='email' value={email} onChange={e => setEmail(e.target.value)}  />
           <input type="password" className={styles.input} placeholder="Password" name='password1' value={password1} onChange={e => setPassword1(e.target.value)}  />
           <input type="password" className={styles.input} placeholder="Confirm Password" name='password2' value={password2} onChange={e => setPassword2(e.target.value)}  />
