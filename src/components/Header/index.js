6ba88cc30c177bdd9f2e213cc650390278cc959e
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-03-02 18:46:15 -0800

    Change header style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import styles from './Header.module.css';
 
-function Header() {
-  return (
-    <>
-    <nav className={styles.nav}>
-      <div>
-        <div className={styles.container}>
-          <img className={styles.logo} src={process.env.PUBLIC_URL + '/images/sandals.png'} alt='church logo'/>
-          <h1>Needs Met</h1>
-        </div>
-      </div>
-      <img className={styles.dots} src={process.env.PUBLIC_URL + '/images/dots.png'} alt='hamburger icon' />
-    </nav>
-    <div className={styles.margin}></div>
-    </> 
-  );
-}
+const Header = () => 
+  <>
+  <h1 className={styles.h1}>NEEDS MET</h1>
+  <div className={styles.margin}></div>
+  </>
+
 
 export default Header;
